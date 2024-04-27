35849fdabddbb7a2ffde4bce1b7f17547e362261
Julio Cesar <jcrj98338259@hotmail.com>
2024-04-27 17:26:24 -0300

    Primeiro commit, subindo repositorio

---
+++ script.js
@@ -0,0 +1,21 @@
+
+function update() {
+    let bill = Number(document.getElementById('yourBill').value)
+    let tipPercent = document.getElementById('tipInput').value
+    let split = Number(document.getElementById('splitInput').value)
+    
+    document.getElementById('tipPercent').innerHTML = tipPercent
+
+    let tip = bill / 100 * tipPercent
+    document.getElementById('tipValue').innerHTML = tip.toFixed(2)
+
+    let tipTotal = tip + bill
+    document.getElementById('totalWithTip').innerHTML = tipTotal.toFixed(2)
+    
+    document.getElementById('splitValue').innerHTML = split
+
+    let billEach = tipTotal / split
+    document.getElementById('billEach').innerHTML = billEach.toFixed(2)
+
+    
+}
