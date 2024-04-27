29b74d584ad959047f4fcdb422c2373796439d60
Julio Cesar <105757450+Lixaum@users.noreply.github.com>
2024-04-27 17:54:08 -0300

    Adição do $

--- script.js
+++ script.js
@@ -4,18 +4,18 @@ function update() {
     let tipPercent = document.getElementById('tipInput').value
     let split = Number(document.getElementById('splitInput').value)
     
-    document.getElementById('tipPercent').innerHTML = tipPercent + '%'
+    document.getElementById('tipPercent').innerHTML = tipPercent +'%'
 
     let tip = bill / 100 * tipPercent
-    document.getElementById('tipValue').innerHTML = tip.toFixed(2)
+    document.getElementById('tipValue').innerHTML = '$' + tip.toFixed(2)
 
     let tipTotal = tip + bill
-    document.getElementById('totalWithTip').innerHTML = tipTotal.toFixed(2)
+    document.getElementById('totalWithTip').innerHTML = '$' + tipTotal.toFixed(2)
     
     document.getElementById('splitValue').innerHTML = split
 
     let billEach = tipTotal / split
-    document.getElementById('billEach').innerHTML = billEach.toFixed(2)
+    document.getElementById('billEach').innerHTML = '$' + billEach.toFixed(2)
 
     
 }
